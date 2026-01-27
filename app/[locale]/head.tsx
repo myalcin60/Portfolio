0a75a2f1e68c7c1ae9c63bb367b7dd09a4c3a17d
yalcin <yalcinmusa60@gamil.com>
2026-01-27 13:36:03 +0100

    head.tsx created

---
+++ app/[locale]/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+    return (
+        <>
+            <meta name="google-site-verification" content="6Sqy_QxP7ZaN7m-PvoDgkvMwaVuySKtcDhhMetqMnyY" />
+        </>
+    );
+}
