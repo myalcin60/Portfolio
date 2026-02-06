76e8c3282ff8afd613777a98ff52dfb54c5d7a8d
yalcin <yalcinmusa60@gamil.com>
2026-02-06 19:54:42 +0100

    sitemap udated

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,7 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://mydevtest.netlify.app/",
+    siteUrl: "https://mydevtest.netlify.app",
     generateRobotsTxt: true,
     changefreq: "weekly",
     priority: 0.8,
+
+    additionalPaths: async (config) => {
+        return [
+            {
+                loc: "/",
+                changefreq: "weekly",
+                priority: 1.0,
+                lastmod: new Date().toISOString(),
+            },
+        ];
+    },
 };
